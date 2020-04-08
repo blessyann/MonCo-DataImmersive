277c9948df56029126dcb56cc6e60f9bd72d74d4
Blessy <blessyannamathew.88@gmail.com>
2020-04-08 17:19:34 -0500

    word_count_assignment

---
+++ assignments/word_count.py
@@ -0,0 +1,9 @@
+with open('word_count.txt') as file_object:
+    lines = file_object.readlines()
+    pi_string = '' 
+    for line in lines:  
+        pi_string += line.strip()    
+        print(pi_string) 
+    print(len(pi_string))
+
+    
