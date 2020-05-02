f7042bad4c198a8c62927008791c22bcebe54276
Blessy Anna Mathew <blessyannamathew.88@gmail.com>
2020-05-02 14:43:04 -0500

    add chapter 2 assignments

---
+++ chapterAssignments/chapter2.py
@@ -0,0 +1,56 @@
+# Exercise 2.1 Simple Message: Store a message in a variable, and then print that message.
+sim_message = "\n Chapter 2.1 exercise starts here "
+print(sim_message)
+
+# Exercise 2.2 Simple Messages: Change the value of the message stored in the variable 
+# from Exercise 2.1, then print the new message.
+sim_message = "\n This message is Exercise 2.2"
+print(sim_message)
+
+# Exercise 2.3 Personal Message: Store a person's name in a variable, and print a message to that 
+# person Your message should be simple such as, "Hello Eric, would you like to learn some Python today?"
+per_message = "Deona"
+print("\nHello " + per_message +", would you like to learn some Python today?")
+
+# Exercise 2.4 Exercise 2.4 - Name Cases: Using the name stored
+#  in the variable from Exercise 2.3, print that person's name in uppercase, lowercase, and titlecase.
+print("\n")
+print(per_message.upper())
+print(per_message.lower())
+print(per_message.title())
+
+
+# Exercise 2.5 Famous Quote: Find a quote from a famous person you admire. Print the quote
+# and the name of the author. Your output should look something like the following, including 
+# the quotation marks: Albert Einstein once said, "A person who never made a mistake never tried 
+# anything new."
+print("\nMahatma Ghandhi once said, " '"Live as if you were to die tomorrow. Learn as if you were to live forever."')
+
+# Exercise 2.6 Famous Quote 2: This time, store the famous person's name in a variable 
+# called famous_person and store the entire message for the quote in a new variable called message.
+# Print your message.
+famous_person = "Mahatma Gandhi"
+message = '"Live as if you were to die tomorrow. Learn as if you were to live forever."'
+print("\n" + famous_person + " once said," +message)
+
+#Exercise 2.8 Number Eight: Write addition, subtraction, multiplication, and division operations
+# that each result in the number 8. Be sure to enclose your operations in print statements to see 
+# the results. You should create four separate lines of code in the program for each operation.
+print(4 + 4)
+print(12 - 4)
+print(4 * 2)
+print(16 / 2)
+
+#Exercise 2.9 Favorite Number: Store your favorite number in a variable. Then using 
+# that variable, create a message that reveals your favorite number. Print the message.
+myfav_num= 8
+print("my favorite numbber is " + str(myfav_num))
+
+"""
+Excise 2.10
+End of chapter 2 exercise
+"""
+
+
+
+
