2c16004becc9e0f206325940531f7a59bdd89ce5
BlessyAnn <59004030+blessyann@users.noreply.github.com>
2020-05-02 18:34:53 -0400

    Chapter 5 Exercise 7

---
+++ chapterAssignments/chapter5_7.py
@@ -0,0 +1,17 @@
+# Exercise 5.7 Favorite Fruit: Make a list of your favorite fruits (call it "favorite_fruits"), and then write five "if" statements 
+# that check for certain fruits in your list (some of the checks should be True and some should be False). If the fruit is in your list, the "if" block should 
+# print a statement such as, "You really like bananas!"
+
+print("\n Favorite fruits")
+favorite_fruits = ['banana','orange','apple','kiwi','pineapple']
+favorite_fruit = 'kiwi'
+if favorite_fruit == 'banana':
+    print(f"I really like {favorite_fruit.title()}")
+if favorite_fruit == 'orange':
+    print(f"I really like {favorite_fruit.title()}")
+if favorite_fruit == 'apple':
+    print(f"I really like {favorite_fruit.title()}")
+if favorite_fruit == 'kiwi':
+    print(f"I really like {favorite_fruit.title()}")
+if favorite_fruit == 'pineapple':
+    print(f"I really like {favorite_fruit.title()}")
